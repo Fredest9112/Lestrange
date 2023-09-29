d65b16fe578ba4ef613215c18a321f39e9f13429
fredest <fredest9112@gmail.com>
2023-09-29 14:21:41 -0500

    Modularizando servicio Zapato

---
+++ Frontend/Lestrange/src/app/Models/zapato.interface.ts
@@ -0,0 +1,9 @@
+export interface Zapato {
+    Id: number;
+    Nombre?: string | null;
+    Descripcion?: string | null;
+    Precio?: number | null;
+    ImagenUrl?: string | null;
+    Stock?: number | null;
+    CategoriaId?: number | null;
+}
