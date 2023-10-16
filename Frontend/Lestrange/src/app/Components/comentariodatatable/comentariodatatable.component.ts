9d7c73bcfd2fde1fc22c263f105fe6bc7945db6f
fredest <fredest9112@gmail.com>
2023-10-16 14:08:33 -0500

    agregando formulario del modelo comentario

--- Frontend/Lestrange/src/app/Components/comentariodatatable/comentariodatatable.component.ts
+++ Frontend/Lestrange/src/app/Components/comentariodatatable/comentariodatatable.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
@@ -8,6 +8,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { Comentario } from 'src/app/Models/comentario.interface';
 import { ComentarioRestService } from 'src/app/Services/rest.comentarioservice';
+import { MatDialog } from '@angular/material/dialog';
+import { ComentarioformComponent } from 'src/app/Forms/comentarioform/comentarioform.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comentariodatatable',
@@ -18,30 +21,30 @@ import { ComentarioRestService } from 'src/app/Services/rest.comentarioservice';
 })
 export class Comentariodatatable implements AfterViewInit {
   dataSource: MatTableDataSource<Comentario> = new MatTableDataSource<Comentario>([]);
-  displayedColumns: string[] = []; // Initialize as an empty array
+  displayedColumns: string[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public restService: ComentarioRestService) {}
+  constructor(public restService: ComentarioRestService, private dialog: MatDialog, private cdRef: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
 
-    // Fetch data and then assign it to the data source
-    this.restService.getComentarioFromRemote().subscribe((comentarios: Comentario[]) => {
+    this.restService.comentariosData$.subscribe((comentarios: Comentario[]) => {
       this.dataSource.data = comentarios;
 
-      // Populate displayedColumns based on the properties of the first user (assuming all users have the same properties)
       if (comentarios.length > 0) {
         this.displayedColumns = Object.keys(comentarios[0]);
-
         this.displayedColumns.push('acciones');
-
-        // Bind the sort to the table dynamically
         this.dataSource.sort = this.sort;
       }
+      this.cdRef.detectChanges(); 
+    });
+
+    this.restService.getComentarioFromRemote().subscribe((comentario: Comentario[]) => {
+      this.restService.updateComentariosData(comentario);
     });
   }
 
@@ -55,10 +58,74 @@ export class Comentariodatatable implements AfterViewInit {
   }
 
   editarComentario(comentario: Comentario) {
-    console.log("editar comentario: "+comentario.texto);
+    this.editComentarioModal("Editar comentario", ComentarioformComponent, comentario);
   }
-  
+
+  agregarComentario(){
+    this.addComentarioModal(ComentarioformComponent);
+  }
+
   borrarComentario(comentario: Comentario) {
-    console.log("borrar comentario: "+comentario.texto);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "Do you want to delete this comentario?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.restService.deleteComentario(comentario.id).subscribe(() => {
+          Swal.fire('Deleted!', 'Your comentario has been deleted.', 'success');
+          // Fetch all zapatos again after successful deletion
+          this.restService.getComentarioFromRemote().subscribe((comentarios: Comentario[]) => {
+            this.restService.updateComentariosData(comentarios);
+          });
+        }, (error) => {
+          Swal.fire('Error', 'There was a problem deleting the comentario.', 'error');
+        });
+      }
+    });
   }
+
+  editComentarioModal(title: string, comentarioForm: any, comentario: Comentario) {
+    const dialogRef = this.dialog.open(comentarioForm, {
+      data: {
+        id: comentario.id,
+        zapatoId: comentario.zapatoId,
+        usuarioId: comentario.usuarioId,
+        texto: comentario.texto,
+        fecha: comentario.fecha
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(updatedComentario => {
+      console.log("updatedComentario: "+updatedComentario);
+      if (updatedComentario) {
+        this.updateLocalDataSource(updatedComentario);
+      }
+    });
+  }
+
+  addComentarioModal(comentarioForm: any) { 
+    const dialogRef = this.dialog.open(comentarioForm, {
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(newComentario => {
+      if (newComentario) {
+        console.log("New comentario added:", newComentario);
+      }
+    });
+   }
+
+  updateLocalDataSource(updatedComentario: Comentario) {
+    const currentData = this.dataSource.data;
+    const index = currentData.findIndex(comentario => comentario.id === updatedComentario.id);
+    if (index > -1) {
+      currentData[index] = updatedComentario;
+      this.restService.updateComentariosData([...currentData]);
+    }
+  }  
 }
--- Frontend/Lestrange/src/app/Services/rest.comentarioservice.ts
+++ Frontend/Lestrange/src/app/Services/rest.comentarioservice.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
 import { Comentario } from '../Models/comentario.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComentarioRestService {
+
+  private comentariosDataSubject = new BehaviorSubject<Comentario[]>([]);
+  comentariosData$ = this.comentariosDataSubject.asObservable();
 
   constructor(public api: HttpClient) { }
 
@@ -17,5 +20,43 @@ export class ComentarioRestService {
         return throwError(error);
       })
     );
+  }
+
+  editComentario(comentario: Comentario): Observable<Comentario> {
+    const editUrl = `https://localhost:7200/api/Comentarios/${comentario.id}`;
+    console.log('Edit URL:', editUrl);
+    console.log('comentario text:', comentario.texto);
+    return this.api.put<Comentario>(editUrl, comentario).pipe(
+      catchError((error: any) => {
+        console.error('An error occurred while editing Comentario:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  addComentario(comentario: Comentario): Observable<Comentario> {
+    const addUrl = `https://localhost:7200/api/Comentarios`;
+    return this.api.post<Comentario>(addUrl, comentario).pipe(
+      catchError((error: any) => {
+        console.error('An error occurred while adding Comentario:', error);
+        console.error('Error Message:', error.message);
+        console.error('Error Status:', error.status);
+        return throwError(error);
+      })
+    );
+  }
+
+  deleteComentario(comentarioId: number): Observable<void> {
+    const deleteUrl = `https://localhost:7200/api/Comentarios/${comentarioId}`;
+    return this.api.delete<void>(deleteUrl).pipe(
+      catchError((error: any) => {
+        console.error('An error occurred while deleting Comentario:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  updateComentariosData(comentarios: Comentario[]) {
+    this.comentariosDataSubject.next(comentarios);
   }
 }
